bbfb06f7b50da485c1d1d4da35a3fcc8dd46690e
rawd93 <syrawd@gmail.com>
2024-02-07 19:15:24 +0300

    notes_view_body

---
+++ notes_app/lib/views/widget/custom _appBar.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:notes_app/views/widget/Search_icon.dart';
+
+class CustomAppBar extends StatelessWidget {
+  const CustomAppBar({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Padding(
+      padding: EdgeInsets.symmetric(horizontal: 16),
+      child:  Row(
+        children: [
+          Text(
+            'Notes',
+            style: TextStyle(fontSize: 26),
+          ),
+          Spacer(),
+           CustomSearchIcon(),
+        ],
+      ),
+    );
+  }
+}
+
