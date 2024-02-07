fa9f8325c5bfe6177141c17abe63f97593056cd5
rawd93 <syrawd@gmail.com>
2024-02-07 19:14:01 +0300

    notes_view

--- notes_app/lib/main.dart
+++ notes_app/lib/main.dart
@@ -1,13 +1,19 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/notes_View.dart';
 
 void main() {
   runApp(const NotesApp());
 }
+
 class NotesApp extends StatelessWidget {
   const NotesApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return MaterialApp(
+      theme: ThemeData(brightness: Brightness.dark
+      ),
+      home: const  NotesView(),
+    );
   }
 }
