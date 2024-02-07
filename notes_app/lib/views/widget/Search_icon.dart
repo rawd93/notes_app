0c689b2cbb1215259bdf974b5a5b1e9c832adc44
rawd93 <syrawd@gmail.com>
2024-02-07 19:16:18 +0300

    search_icon

---
+++ notes_app/lib/views/widget/Search_icon.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class CustomSearchIcon extends StatelessWidget {
+  const CustomSearchIcon({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      height: 50,
+      width: 50,
+      decoration: BoxDecoration(
+        borderRadius: BorderRadius.circular(16),
+        color: Colors.white.withOpacity(0.2),
+      ),
+      child: const Icon(
+        Icons.search,
+        size: 40,
+      ),
+    );
+  }
+}
