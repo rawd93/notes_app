8934a7837481a6b8b39af0f8c24fdc314bbdd693
rawd93 <syrawd@gmail.com>
2024-02-08 20:16:50 +0300

    notes_app

--- notes_app/lib/views/notes_view_body .dart
+++ notes_app/lib/views/notes_view_body .dart
@@ -1,17 +1,24 @@
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 import 'package:notes_app/views/widget/custom%20_appBar.dart';
+import 'package:notes_app/views/widget/custom_notes_Iteam.dart';
 
 class NotesViewBody extends StatelessWidget {
   const NotesViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const  Column(
+    return const Column(
       children: [
         SizedBox(
           height: 40,
         ),
         CustomAppBar(),
+        SizedBox(
+          height: 20,
+        ),
+        NotesIteam(),
       ],
     );
   }
