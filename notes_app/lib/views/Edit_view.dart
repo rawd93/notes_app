906e9aa683ca3c3097762ddc1ba06fa8117acd26
rawd93 <syrawd@gmail.com>
2024-02-09 21:43:26 +0300

    create edit note view

---
+++ notes_app/lib/views/Edit_view.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class EditNotesView extends StatelessWidget {
+  const EditNotesView({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Scaffold(
+      body: EditNotesViewBody(),
+    );
+  }
+}
+
+class EditNotesViewBody extends StatelessWidget {
+  const EditNotesViewBody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
