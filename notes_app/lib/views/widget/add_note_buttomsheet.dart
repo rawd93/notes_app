bb5022dcc949decb47e515cc2d017d20c30436f6
rawd93 <syrawd@gmail.com>
2024-02-09 21:25:36 +0300

    finish notes bottom sheet

--- notes_app/lib/views/widget/add_note_buttomsheet.dart
+++ notes_app/lib/views/widget/add_note_buttomsheet.dart
@@ -7,21 +7,59 @@ class AddNoteBottomSheet extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return  const Padding(
+    return const Padding(
       padding: EdgeInsets.symmetric(vertical: 16),
-      child: Column(
-        children: [
-          CustomTextFieled(
-            hint: 'Title',
+      child: SingleChildScrollView(
+        child: Column(
+          children: [
+            SizedBox(
+              height: 32,
+            ),
+            CustomTextFieled(
+              hint: 'Title',
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            CustomTextFieled(
+              hint: 'Content',
+              maxLines: 5,
+            ),
+            SizedBox(
+              height: 16,
+            ),
+            CustomButton(),
+            SizedBox(
+              height: 16,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class CustomButton extends StatelessWidget {
+  const CustomButton({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        decoration: BoxDecoration(
+          color: kprimaryColor,
+          borderRadius: BorderRadius.circular(8),
+        ),
+        height: 55,
+        width: MediaQuery.of(context).size.width,
+        child: const Center(
+          child: Text(
+            'Add',
+            style: TextStyle(
+                fontSize: 25, color: Colors.black, fontWeight: FontWeight.bold),
           ),
-          SizedBox(
-            height: 24,
-          ),
-          CustomTextFieled(
-            hint: 'Content',
-            maxLines: 5,
-          ),
-        ],
+        ),
       ),
     );
   }
