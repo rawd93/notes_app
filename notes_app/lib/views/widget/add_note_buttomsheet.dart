78f074a65dd73e39a20c04586d76d7bd59cebdc0
rawd93 <syrawd@gmail.com>
2024-02-09 20:17:55 +0300

    add fieled to bottom sheet

--- notes_app/lib/views/widget/add_note_buttomsheet.dart
+++ notes_app/lib/views/widget/add_note_buttomsheet.dart
@@ -1,12 +1,28 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/widget/constant.dart';
+import 'package:notes_app/views/widget/custom_Textfield.dart';
 
 class AddNoteBottomSheet extends StatelessWidget {
   const AddNoteBottomSheet({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return  const Padding(
+      padding: EdgeInsets.symmetric(vertical: 16),
+      child: Column(
+        children: [
+          CustomTextFieled(
+            hint: 'Title',
+          ),
+          SizedBox(
+            height: 24,
+          ),
+          CustomTextFieled(
+            hint: 'Content',
+            maxLines: 5,
+          ),
+        ],
+      ),
     );
   }
 }
