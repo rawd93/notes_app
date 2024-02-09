bcef23215739a8efce2f9dd122c45c6074ec58b6
rawd93 <syrawd@gmail.com>
2024-02-09 21:44:21 +0300

    navigator noteview

--- notes_app/lib/views/widget/custom_notes_Iteam.dart
+++ notes_app/lib/views/widget/custom_notes_Iteam.dart
@@ -1,56 +1,67 @@
 import 'package:flutter/material.dart';
+import 'package:notes_app/views/Edit_view.dart';
 
 class NotesIteam extends StatelessWidget {
   const NotesIteam({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.only(left: 16, right: 16),
-      child: Container(
-        decoration: BoxDecoration(
-          borderRadius: BorderRadius.circular(16),
-          color: const Color(0xffFFD57F),
-        ),
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.end,
-          children: [
-            ListTile(
-              title: const Padding(
-                padding: EdgeInsets.only(top: 24, bottom: 8),
-                child: Text(
-                  'Flutter tips',
-                  style: TextStyle(
-                    color: Colors.black,
-                    fontSize: 31,
+    return GestureDetector(
+      onTap: () {
+        Navigator.push(context, MaterialPageRoute(
+          builder: (Context) {
+            return const EditNotesView();
+          },
+        ));
+      },
+      child: Padding(
+        padding: const EdgeInsets.only(left: 16, right: 16),
+        child: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(16),
+            color: const Color(0xffFFD57F),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.end,
+            children: [
+              ListTile(
+                title: const Padding(
+                  padding: EdgeInsets.only(top: 24, bottom: 8),
+                  child: Text(
+                    'Flutter tips',
+                    style: TextStyle(
+                      color: Colors.black,
+                      fontSize: 31,
+                    ),
                   ),
                 ),
+                subtitle: Padding(
+                  padding: const EdgeInsets.only(top: 16, bottom: 16),
+                  child: Text(
+                    'Build Your Career '
+                    'with Rawd Sayed',
+                    style: TextStyle(
+                        color: Colors.black.withOpacity(0.4), fontSize: 25),
+                  ),
+                ),
+                trailing: IconButton(
+                  onPressed: () {},
+                  icon: const Icon(Icons.delete, color: Colors.black, size: 30),
+                ),
               ),
-              subtitle: Padding(
-                padding: const EdgeInsets.only(top: 16, bottom: 16),
+              Padding(
+                padding:
+                    const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
                 child: Text(
-                  'Build Your Career '
-                  'with Rawd Sayed',
+                  'May21,2023',
                   style: TextStyle(
-                      color: Colors.black.withOpacity(0.4), fontSize: 25),
+                    color: Colors.black.withOpacity(0.4),
+                    fontSize: 20,
+                  ),
                 ),
-              ),
-              trailing: IconButton(
-                onPressed: () {},
-                icon: const Icon(Icons.delete, color: Colors.black, size: 30),
-              ),
-            ),
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 16),
-              child: Text(
-                'May21,2023',
-                style: TextStyle(
-                  color: Colors.black.withOpacity(0.4),
-                  fontSize: 20,
-                ),
-              ),
-            )
-          ],
+              )
+            ],
+          ),
         ),
       ),
     );
