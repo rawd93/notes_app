c3465b69111e0913be1deef301c0fe543c7f5147
rawd93 <syrawd@gmail.com>
2024-02-10 19:58:13 +0300

    Search icon

--- notes_app/lib/views/widget/Search_icon.dart
+++ notes_app/lib/views/widget/Search_icon.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
 
-class CustomSearchIcon extends StatelessWidget {
-  const CustomSearchIcon({super.key});
+class CustomIcon extends StatelessWidget {
+  const CustomIcon({super.key, required this.icon});
+  final IconData icon;
 
   @override
   Widget build(BuildContext context) {
@@ -12,9 +13,12 @@ class CustomSearchIcon extends StatelessWidget {
         borderRadius: BorderRadius.circular(16),
         color: Colors.white.withOpacity(0.2),
       ),
-      child: const Icon(
-        Icons.search,
-        size: 40,
+      child: IconButton(
+        onPressed: () {},
+        icon: Icon(
+          icon,
+          size: 28,
+        ),
       ),
     );
   }
