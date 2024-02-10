bc659a660b14c72d2174e4ed4e8c65028c428338
rawd93 <syrawd@gmail.com>
2024-02-10 19:57:27 +0300

    custom aapbar

--- notes_app/lib/views/widget/custom _appBar.dart
+++ notes_app/lib/views/widget/custom _appBar.dart
@@ -2,20 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:notes_app/views/widget/Search_icon.dart';
 
 class CustomAppBar extends StatelessWidget {
-  const CustomAppBar({super.key});
+  const CustomAppBar({super.key, required this.title, required this.icon});
+  final String title;
+  final IconData icon;
 
   @override
   Widget build(BuildContext context) {
-    return const Padding(
-      padding: EdgeInsets.symmetric(horizontal: 16),
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 16),
       child: Row(
         children: [
           Text(
-            'Notes',
-            style: TextStyle(fontSize: 26),
+            title,
+            style: const TextStyle(fontSize: 26),
           ),
-          Spacer(),
-          CustomSearchIcon(),
+          const Spacer(),
+          CustomIcon(icon: icon),
         ],
       ),
     );
