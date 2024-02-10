bd680ee7d7aa16f093de26e47d4d2f1902554664
rawd93 <syrawd@gmail.com>
2024-02-10 21:48:52 +0300

    create note model

---
+++ notes_app/lib/views/widget/Models/note_model.dart
@@ -0,0 +1,12 @@
+class NoteModel {
+  String title;
+  String subTitle;
+  String date;
+  int color;
+
+  NoteModel(
+      {required this.title,
+      required this.subTitle,
+      required this.date,
+      required this.color});
+}
